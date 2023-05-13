00701e9e9931f76ea68fcdb2ea1779bb49ef1339
erixweb <erikmorenodev@gmail.com>
2023-05-13 15:37:30 +0200

    Allow Multiplayer using SessionStorage (Insecure)

--- socket.js
+++ socket.js
@@ -4,22 +4,57 @@ const fs = require("fs")
 const wss = new WebSocket.Server({
     port: 8080
 })
+let i = 0
+const spawnPlayers = () => {
+    wss.clients.forEach((client) => {
+        i = 0   
+        client.send(`resetPlayerPkg`)
+        wss.clients.forEach((c) => {
+            c.send(`controllerID: ${i}`)
+            i++
+        })
+        client.send(`createPlayerPkg: erik, 55, ${i}`)
+    })
+}
 wss.on("connection", ws => {
+    fs.readFile("list.txt", "utf-8", (err, res) => {
+        ws.send(`increaseClicksPkg: ${res}`)
+        console.log(res)
+    })
+    spawnPlayers()
     ws.on("message", async (data) => {
         if (data == "INCREASE") {
             await fs.readFile("list.txt", "utf-8", (err, res) => {
                 fs.writeFileSync("list.txt", `${(parseInt(res)+1).toString()}`)
 
                 wss.clients.forEach((client) => {
-                    client.send(res)
+                    client.send(`increaseClicksPkg: ${res}`)
                 })
             })
-        } else {
+        } else if (data == "PING") {
             await fs.readFile("list.txt", "utf-8", (err, res) => {
-                ws.send(res)
-
-                console.log(res)
+                ws.send(`increaseClicksPkg: ${res}`)
             })
+        } else if (data.toString().startsWith("movePlayerPkg: ")) {
+            let formatted = data.toString().replace("movePlayerPkg: ", "")
+
+            formatted = formatted.split(",")
+            const playerID = formatted[0]
+            const playerMovement = formatted[1]
+
+            if (parseInt(playerMovement) == 10) {
+                wss.clients.forEach((client) => {
+                    client.send(`movePlayerPkg: ${playerID}, ${playerMovement}`)
+                })
+            }
         }
+    })
+    ws.on("close", () => {
+        i = 0
+        wss.clients.forEach((client) => {
+            client.send(`resetPlayerPkg`)
+            spawnPlayers()
+            i++
+        })
     })
 })
